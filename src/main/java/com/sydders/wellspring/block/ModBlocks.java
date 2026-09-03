773b2d756585fd8699863d85c19156d99b59d4f4
SyddersLMAO <sydneyewens06@gmail.com>
2026-09-03 20:16:42 +0100

    added block register function

--- src/main/java/com/sydders/wellspring/block/ModBlocks.java
+++ src/main/java/com/sydders/wellspring/block/ModBlocks.java
@@ -1,12 +1,20 @@
 package com.sydders.wellspring.block;
 
 import com.sydders.wellspring.Wellspring;
+import com.sydders.wellspring.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(Wellspring.MODID);
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.registerItem(name, properties -> new BlockItem(block.get(), properties.useBlockDescriptionPrefix()));
+    }
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
