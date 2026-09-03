b4045395633affa463f0081d6ceb21c7aa588c09
SyddersLMAO <sydneyewens06@gmail.com>
2026-09-03 11:09:53 +0100

    fixed ruby item

--- src/main/java/com/sydders/wellspring/Wellspring.java
+++ src/main/java/com/sydders/wellspring/Wellspring.java
@@ -1,5 +1,6 @@
 package com.sydders.wellspring;
 
+import com.sydders.wellspring.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -45,6 +46,9 @@ public class Wellspring {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
+        // Register Items
+        ModItems.register(modEventBus);
+
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (Wellspring) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
@@ -63,7 +67,9 @@ public class Wellspring {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.RUBY);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
